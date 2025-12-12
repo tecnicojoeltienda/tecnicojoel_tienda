4d2b9a84e328a84f47213473cd9cd74b6741a023
Nunu-DMG09 <davidmesta09@gmail.com>
2025-12-12 18:17:19 -0500

    add SSL configuration to database connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,6 +8,10 @@ export const conexion = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,  
+  ssl: {
+    rejectUnauthorized: false 
+  }
 });
 
 conexion
