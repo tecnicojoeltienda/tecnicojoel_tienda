37c2e4da174521bfe457afdbda942d3ecccdd350
Nunu-DMG09 <davidmesta09@gmail.com>
2025-12-12 17:45:26 -0500

    update database connection configuration

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,17 +1,17 @@
-
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-// Crear una conexión (no pool) y exportarla como `conexion`.
-// Nota: `mysql.createConnection(...)` devuelve una Promise, así que `conexion` será una Promise
-// que resuelve a la conexión. En los módulos que la consuman debes `await`arla.
 export const conexion = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,  
+  ssl: {
+    rejectUnauthorized: false 
+  }
 });
 
 conexion
