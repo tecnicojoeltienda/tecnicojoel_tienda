8229629bd1d9cff8570770fc607aa843e3760128
Nunu-DMG09 <davidmesta09@gmail.com>
2025-12-12 17:54:01 -0500

    remove unused database connection options

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,10 +8,6 @@ export const conexion = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,  
-  ssl: {
-    rejectUnauthorized: false 
-  }
 });
 
 conexion
